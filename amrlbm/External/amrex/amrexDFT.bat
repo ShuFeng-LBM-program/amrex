fd687607c903f0c216a418bb8c2ca08ebbee7e4e
zhanglili <dimitri.zhanglili@foxmail.com>
2026-01-09 16:47:40 +0800

    Modify multiGPU cmake

--- amrlbm/External/amrex/amrexDFT.bat
+++ amrlbm/External/amrex/amrexDFT.bat
@@ -4,7 +4,7 @@ set "LibName=lib_DFT"
 
 mkdir %LibName%
 
-cmake -S ./Source -B ./%LibName%  -DCMAKE_BUILD_TYPE=Release -DAMReX_MPI=OFF -DAMReX_FORTRAN=OFF   -DAMReX_EB=EB  -DAMReX_PARTICLES=OFF  
+cmake -S ./Source -B ./%LibName% -DAMReX_MPI=OFF  -DCMAKE_BUILD_TYPE=Release  -DAMReX_FORTRAN=OFF   -DAMReX_EB=EB  -DAMReX_PARTICLES=OFF   -DAMReX_LINEAR_SOLVERS_EM=OFF -DAMReX_AMRLEVEL=OFF -DAMReX_LINEAR_SOLVERS_INCFLO=OFF -DAMReX_LINEAR_SOLVERS=OFF  
 
 cmake --build %LibName% --config Release -j 32
 
--- amrlbm/External/amrex/amrexMPI.bat
+++ amrlbm/External/amrex/amrexMPI.bat
@@ -4,7 +4,7 @@ set "LibName=lib_MPI"
 
 mkdir %LibName%
 
-cmake -S ./Source -B ./%LibName%  -DCMAKE_BUILD_TYPE=Release  -DAMReX_FORTRAN=OFF   -DAMReX_EB=EB  -DAMReX_PARTICLES=OFF  
+cmake -S ./Source -B ./%LibName%  -DCMAKE_BUILD_TYPE=Release  -DAMReX_FORTRAN=OFF   -DAMReX_EB=EB  -DAMReX_PARTICLES=OFF   -DAMReX_LINEAR_SOLVERS_EM=OFF -DAMReX_AMRLEVEL=OFF -DAMReX_LINEAR_SOLVERS_INCFLO=OFF -DAMReX_LINEAR_SOLVERS=OFF  
 
 cmake --build %LibName% --config Release -j 32
 
